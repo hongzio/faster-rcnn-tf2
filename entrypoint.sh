9dbe5a1ca7eaa30a629f19ba5f010956b6201985
Hongzio <hongzio@user.noreply.github.com>
2019-06-11 10:57:29 +0900

    ✨ Init project

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+/usr/sbin/sshd
+/usr/local/bin/tensorboard --logdir /tmp/summaries --port 6006
---
+++ train.py
@@ -0,0 +1,8 @@
+from frcnn.faster_rcnn import FasterRCNN
+from frcnn.config import Config
+
+
+if __name__ == '__main__':
+    cfg = Config()
+    faster_rcnn = FasterRCNN(cfg)
+    faster_rcnn.train()
