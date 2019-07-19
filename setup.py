266e5cd32a70da15ab24dcb915a49549b9bf50c7
Hongzio <hongzio@hotmail.com>
2019-07-19 23:55:51 +0900

    Packaging

--- setup.py
+++ setup.py
@@ -22,4 +22,7 @@ setup(name='faster-rcnn',
 
       setup_requires=[],
 
-      install_requires=['tensorflow-gpu==2.0.0-beta1'])
+      install_requires=['tensorflow-gpu==2.0.0-beta1'],
+
+      include_package_data=True
+      )
