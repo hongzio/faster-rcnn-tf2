6be2847ccd84eb565d73011f1b358f37bcd84df1
Hongzio <hongzio@hotmail.com>
2019-07-22 12:46:44 +0900

    Packaging

--- frcnn/config/config.py
+++ frcnn/config/config.py
@@ -7,10 +7,10 @@ class Config:
     def __init__(self, additional_config_path=None):
         default_config = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'default.yml')
         with open(default_config, 'r') as f:
-            self.config = yaml.load(f, Loader=yaml.loader.FullLoader)
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
         if additional_config_path is not None:
             with open(additional_config_path, 'r') as f:
-                self.config.update(yaml.load(f, Loader=yaml.loader.FullLoader))
+                self.config.update(yaml.load(f, Loader=yaml.FullLoader))
 
     def __getitem__(self, item):
         return self.config[item]
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='faster-rcnn',
 
       setup_requires=[],
 
-      install_requires=['tensorflow-gpu==2.0.0-beta1', 'PyYAML'],
+      install_requires=['tensorflow-gpu==2.0.0-beta1', 'PyYAML==5.1.1'],
 
       include_package_data=True
       )
