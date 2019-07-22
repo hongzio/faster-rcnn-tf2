13cdf9bb203f87fce89551c47d50f934f9f70cfd
Hongzio <hongzio@hotmail.com>
2019-07-22 12:44:57 +0900

    Packaging

--- frcnn/faster_rcnn.py
+++ frcnn/faster_rcnn.py
@@ -22,7 +22,7 @@ class FasterRCNN:
                                      )
         self.dataset = None
         self.train_loss = tf.keras.metrics.Mean(name='train_loss')
-        self.optimizer = tf.keras.optimizers.Adam(1e-6)
+        self.optimizer = tf.keras.optimizers.Adam(1e-4)
 
 
     def init_train_context(self):
@@ -37,7 +37,7 @@ class FasterRCNN:
                                                                         self.config['train']['max_num_gt_boxes'])))
         self.dataset = dataset.batch(self.config['train']['rpn_batch_size'])
 
-    # @tf.function
+    @tf.function
     def _train_step(self, x, rpn_y, gt_boxes):
         with tf.GradientTape() as tape:
             loss = self.model(x, rpn_y, gt_boxes)
