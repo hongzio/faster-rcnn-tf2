3b19bb5a4e39b6257563a8194bf778341eb70280
Hongzio <hongzio@hotmail.com>
2019-07-24 00:42:40 +0900

    Packaging

--- frcnn/faster_rcnn.py
+++ frcnn/faster_rcnn.py
@@ -45,7 +45,6 @@ class FasterRCNN:
                                                                         self.config['train']['min_iou'],
                                                                         self.config['train']['max_iou'],
                                                                         self.config['train']['max_num_gt_boxes'])))
-        dataset = dataset.prefetch(self.config['train']['rpn_batch_size'])
         self.dataset = dataset.batch(self.config['train']['rpn_batch_size'])
 
         self.train_loss = tf.keras.metrics.Mean(name='train_loss')
